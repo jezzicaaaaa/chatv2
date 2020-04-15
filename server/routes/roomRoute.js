6deb419c457e6b8a5cc498dac78041942b9f1b9c
jezzicaaaaa <jezzicamae@gmail.com>
2020-04-14 23:24:24 -0400

    fixed edit and delete function handling

--- server/routes/roomRoute.js
+++ server/routes/roomRoute.js
@@ -5,37 +5,32 @@ const router = express.Router();
 const middleware = require('../middleware');
 
 // Get Rooms
-router.route('/').get(async (req, res) => {	
+router.route('/').get(async (req, res) => {
 	try {
 		const room = await Rooms.find({});
 		res.json(room);
-
 	} catch (err) {
-		res.json({message: err});
+		res.json({ message: err });
 	}
 });
 
 // Get Specific Room
-router.route('/:id').get(async (req, res) => {	
+router.route('/:id').get(async (req, res) => {
 	try {
-		const room = await Rooms.findById({_id: req.params.id});
+		const room = await Rooms.findById({ _id: req.params.id });
 		res.json(room);
-
 	} catch (err) {
-		res.json({message: err});
+		res.json({ message: err });
 	}
 });
 
 // Delete Room
 router.route('/:id').delete(async (req, res) => {
 	try {
-		const removeRoom = await Rooms.findByIdAndRemove(
-			{_id: req.params.id}
-		);
+		const removeRoom = await Rooms.findByIdAndRemove({ _id: req.params.id });
 		res.json(removeRoom);
-
 	} catch (err) {
-		res.json( {message: err})
+		res.json({ message: err });
 	}
 });
 
@@ -44,24 +39,12 @@ router.route('/:id').patch(async (req, res) => {
 	try {
 		const updatedRoom = await Rooms.updateOne(
 			{ _id: req.params.id },
-			{ $set: {roomname: req.body.roomname, status: req.body.status} }
+			{ $set: { roomname: req.body.roomname, status: req.body.status, edited: Date.now() } }
 		);
 		res.json(updatedRoom);
-
 	} catch (err) {
-		res.json( {message: err});
+		res.json({ message: err });
 	}
 });
-
-// Old Get Rooms
-// router.route('/').get((req, res, next) => {
-// 	res.statusCode = 200;
-
-// 	connectdb.then((db) => {
-// 		Rooms.find({}).then((room) => {
-// 			res.send(room);
-// 		});
-// 	});	
-// });
 
 module.exports = router;
